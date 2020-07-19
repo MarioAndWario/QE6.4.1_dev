4716327acf936723ab96cc035822b8103cd597e4
wu2meng3 <wu1meng2@berkeley.edu>
2020-07-19 15:20:36 -0700

    add makefiles on cori2

--- PW/src/setup.f90
+++ PW/src/setup.f90
@@ -678,7 +678,7 @@ LOGICAL FUNCTION check_para_diag( nbnd )
         ELSE
            CALL errore( 'setup','Unexpected sub-group communicator ', 1 )
         END IF
-#if defined(__ELPA) || defined(__ELPA_2015) || defined(__ELPA_2016)
+#if defined(__ELPA) || defined(__ELPA_2015) || defined(__ELPA_2016) || defined(__ELPA_2017)
         WRITE( stdout, '(5X,"ELPA distributed-memory algorithm ", &
               & "(size of sub-group: ", I2, "*", I3, " procs)",/)') &
                np_ortho(1), np_ortho(2)
--- include/configure.h
+++ include/configure.h
@@ -18,12 +18,12 @@
 
 #define   __CONF_CC             "gcc"
 #define   __CONF_CFLAGS         "-O3"
-#define   __CONF_DFLAGS         " -D__DFTI -D__MPI"
+#define   __CONF_DFLAGS         " -D__DFTI"
 #define   __CONF_CPP            "cpp"
 #define   __CONF_CPPFLAGS       "-P -traditional"
 
 #define   __CONF_F90            "ifort"
-#define   __CONF_MPIF90         "mpif90"
+#define   __CONF_MPIF90         "ifort"
 #define   __CONF_F90FLAGS       "$(FFLAGS) -nomodule"
 #define   __CONF_F77            "ifort"
 #define   __CONF_FFLAGS         "-O2 -assume byterecl -g -traceback"
@@ -31,7 +31,7 @@
 #define   __CONF_PRE_FDFLAGS    "-fpp "
 #define   __CONF_FDFLAGS        "$(DFLAGS) $(MANUAL_DFLAGS)"
 
-#define   __CONF_LD             "mpif90"
+#define   __CONF_LD             "ifort"
 #define   __CONF_LDFLAGS        " "
 #define   __CONF_IMOD           "-I"
 
