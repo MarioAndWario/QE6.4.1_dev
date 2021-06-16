61614372632db5f4723071a477a226e51603b392
wu2meng3 <wu1meng2@berkeley.edu>
2021-06-15 22:22:38 -0700

    write_cd can output

--- PP/src/bgw2pw.f90
+++ PP/src/bgw2pw.f90
@@ -198,15 +198,15 @@ PROGRAM bgw2pw
      IF ( ionode ) WRITE ( 6, '(5x,"done write_evc",/)' )
   ENDIF
 
-  ! IF ( rhog_flag ) THEN
-  !   input_file_name = TRIM ( tmp_dir ) // TRIM ( rhog_file )
-  !   output_dir_name = TRIM ( tmp_dir ) // TRIM ( prefix ) // '.save'
-  !   IF ( ionode ) WRITE ( 6, '(5x,"call write_cd")' )
-  !   CALL start_clock ( 'write_cd' )
-  !   CALL write_cd ( input_file_name, real_or_complex, output_dir_name )
-  !   CALL stop_clock ( 'write_cd' )
-  !   IF ( ionode ) WRITE ( 6, '(5x,"done write_cd",/)' )
-  ! ENDIF
+  IF ( rhog_flag ) THEN
+    input_file_name = TRIM ( tmp_dir ) // TRIM ( rhog_file )
+    output_dir_name = TRIM ( tmp_dir ) // TRIM ( prefix ) // '.save'
+    IF ( ionode ) WRITE ( 6, '(5x,"call write_cd")' )
+    CALL start_clock ( 'write_cd' )
+    CALL write_cd ( input_file_name, real_or_complex, output_dir_name )
+    CALL stop_clock ( 'write_cd' )
+    IF ( ionode ) WRITE ( 6, '(5x,"done write_cd",/)' )
+  ENDIF
 
   IF ( ionode ) WRITE ( 6, * )
   IF ( wfng_flag ) CALL print_clock ( 'write_evc' )
@@ -626,7 +626,7 @@ CONTAINS
     CALL open_buffer(iunwfc, 'wfc', nwordwfc, +1, exst_mem, exst)
 
     ! Write everything again with the new prefix
-    CALL write_scf(rho, nspin)
+    ! CALL write_scf(rho, nspin)
 
     write(*,*) "nk = ", nk
 
@@ -788,256 +788,259 @@ CONTAINS
 
   !-------------------------------------------------------------------------------
 
-  ! SUBROUTINE write_cd ( input_file_name, real_or_complex, output_dir_name )
+  SUBROUTINE write_cd ( input_file_name, real_or_complex, output_dir_name )
 
-  !   USE cell_base, ONLY : omega, alat, tpiba, at, bg
-  !   USE constants, ONLY : eps6
-  !   USE fft_base, ONLY : dfftp
-  !   USE fft_interfaces, ONLY : invfft
-  !   USE gvect, ONLY : ngm, ngm_g, ig_l2g, mill
-  !   USE io_global, ONLY : ionode, ionode_id
-  !   USE ions_base, ONLY : nat
-  !   USE xml_io_base, ONLY : write_rho
-  !   USE iotk_module, ONLY : iotk_attlenx, iotk_free_unit, iotk_open_write, &
-  !     iotk_write_begin, iotk_write_attr, iotk_write_empty, iotk_write_dat, &
-  !     iotk_write_end, iotk_close_write
-  !   USE kinds, ONLY : DP
-  !   USE lsda_mod, ONLY : nspin
-  !   USE mp, ONLY : mp_bcast, mp_sum
-  !   USE mp_pools, ONLY : intra_pool_comm
-  !   USE mp_world, ONLY : world_comm
-  !   USE scf, ONLY : rho
-  !   USE symm_base, ONLY : s, nsym
-  !   USE wavefunctions, ONLY : psic
+    USE cell_base, ONLY : omega, alat, tpiba, at, bg
+    USE constants, ONLY : eps6
+    USE fft_base, ONLY : dfftp
+    USE fft_interfaces, ONLY : invfft
+    USE gvect, ONLY : ngm, ngm_g, ig_l2g, mill
+    USE io_global, ONLY : ionode, ionode_id
+    USE ions_base, ONLY : nat
+    ! USE xml_io_base, ONLY : write_rho    
+    ! USE iotk_module, ONLY : iotk_attlenx, iotk_free_unit, iotk_open_write, &
+    !   iotk_write_begin, iotk_write_attr, iotk_write_empty, iotk_write_dat, &
+    !   iotk_write_end, iotk_close_write
+    USE io_rho_xml,         ONLY : write_scf    
+    USE kinds, ONLY : DP
+    USE lsda_mod, ONLY : nspin
+    USE mp, ONLY : mp_bcast, mp_sum
+    USE mp_pools, ONLY : intra_pool_comm
+    USE mp_world, ONLY : world_comm
+    USE scf, ONLY : rho
+    USE symm_base, ONLY : s, nsym
+    USE wavefunctions, ONLY : psic
 
-  !   IMPLICIT NONE
+    IMPLICIT NONE
 
-  !   character ( len = 256 ), intent ( in ) :: input_file_name
-  !   integer, intent ( in ) :: real_or_complex
-  !   character ( len = 256 ), intent ( in ) :: output_dir_name
+    character ( len = 256 ), intent ( in ) :: input_file_name
+    integer, intent ( in ) :: real_or_complex
+    character ( len = 256 ), intent ( in ) :: output_dir_name
 
-  !   logical :: f1, f2
-  !   integer :: i, j, iu, is, ig, jg, fg, ir, ns, ng, &
-  !     ntran, cell_symmetry, na, irecord, nrecord, ng_irecord, nr ( 3 )
-  !   real ( DP ) :: ecutrho, celvol, recvol, al, bl, xdel, &
-  !     a ( 3, 3 ), b ( 3, 3 ), adot ( 3, 3 ), bdot ( 3, 3 )
-  !   character :: sdate*32, stime*32, stitle*32
-  !   character ( len = 256 ) :: filename
-  !   character ( iotk_attlenx ) :: attr
+    logical :: f1, f2
+    integer :: i, j, iu, is, ig, jg, fg, ir, ns, ng, &
+      ntran, cell_symmetry, na, irecord, nrecord, ng_irecord, nr ( 3 )
+    real ( DP ) :: ecutrho, celvol, recvol, al, bl, xdel, &
+      a ( 3, 3 ), b ( 3, 3 ), adot ( 3, 3 ), bdot ( 3, 3 )
+    character :: sdate*32, stime*32, stitle*32
+    character ( len = 256 ) :: filename
+    character ( iotk_attlenx ) :: attr
 
-  !   integer, allocatable :: gvec ( :, : )
-  !   real ( DP ), allocatable :: rhogr ( :, : )
-  !   complex ( DP ), allocatable :: rhogc ( :, : )
-  !   complex ( DP ), allocatable :: rhog ( :, : )
-  !   complex ( DP ), allocatable :: rhog_unsrt ( :, : )
+    integer, allocatable :: gvec ( :, : )
+    real ( DP ), allocatable :: rhogr ( :, : )
+    complex ( DP ), allocatable :: rhogc ( :, : )
+    complex ( DP ), allocatable :: rhog ( :, : )
+    complex ( DP ), allocatable :: rhog_unsrt ( :, : )
 
-  !   CALL check_inversion ( real_or_complex, nsym, s, nspin, .true., .true. )
+    CALL check_inversion ( real_or_complex, nsym, s, nspin, .true., .true. )
 
-  !   IF ( ionode ) CALL iotk_free_unit ( iu )
+    ! IF ( ionode ) CALL iotk_free_unit ( iu )
 
-  !   IF ( ionode ) THEN
-  !     OPEN ( unit = iu, file = TRIM ( input_file_name ), &
-  !       form = 'unformatted', status = 'old' )
-  !     READ ( iu ) stitle, sdate, stime
-  !   ENDIF
+    IF ( ionode ) THEN
+      OPEN ( unit = iu, file = TRIM ( input_file_name ), &
+        form = 'unformatted', status = 'old' )
+      READ ( iu ) stitle, sdate, stime
+    ENDIF
 
-  !   CALL mp_bcast ( stitle, ionode_id, world_comm )
-  !   f1 = real_or_complex == 1 .AND. stitle(1:8) == 'RHO-Real'
-  !   f2 = real_or_complex == 2 .AND. stitle(1:11) == 'RHO-Complex'
-  !   IF ( ( .NOT. f1 ) .AND. ( .NOT. f2 ) ) &
-  !     CALL errore ( 'write_cd', 'file header', 1 )
+    CALL mp_bcast ( stitle, ionode_id, world_comm )
+    f1 = real_or_complex == 1 .AND. stitle(1:8) == 'RHO-Real'
+    f2 = real_or_complex == 2 .AND. stitle(1:11) == 'RHO-Complex'
+    IF ( ( .NOT. f1 ) .AND. ( .NOT. f2 ) ) &
+      CALL errore ( 'write_cd', 'file header', 1 )
 
-  !   IF ( ionode ) THEN
-  !     READ ( iu ) ns, ng, ntran, cell_symmetry, na, ecutrho
-  !     READ ( iu ) ( nr ( ir ), ir = 1, 3 )
-  !     READ ( iu ) celvol, al, ( ( a ( j, i ), j = 1, 3 ), i = 1, 3 ), &
-  !       ( ( adot ( j, i ), j = 1, 3 ), i = 1, 3 )
-  !     READ ( iu ) recvol, bl, ( ( b ( j, i ), j = 1, 3 ), i = 1, 3 ), &
-  !       ( ( bdot ( j, i ), j = 1, 3 ), i = 1, 3 )
-  !   ENDIF
+    IF ( ionode ) THEN
+      READ ( iu ) ns, ng, ntran, cell_symmetry, na, ecutrho
+      READ ( iu ) ( nr ( ir ), ir = 1, 3 )
+      READ ( iu ) celvol, al, ( ( a ( j, i ), j = 1, 3 ), i = 1, 3 ), &
+        ( ( adot ( j, i ), j = 1, 3 ), i = 1, 3 )
+      READ ( iu ) recvol, bl, ( ( b ( j, i ), j = 1, 3 ), i = 1, 3 ), &
+        ( ( bdot ( j, i ), j = 1, 3 ), i = 1, 3 )
+    ENDIF
 
-  !   CALL mp_bcast ( ns, ionode_id, world_comm )
-  !   CALL mp_bcast ( ng, ionode_id, world_comm )
-  !   CALL mp_bcast ( ntran, ionode_id, world_comm )
-  !   CALL mp_bcast ( cell_symmetry, ionode_id, world_comm )
-  !   CALL mp_bcast ( na, ionode_id, world_comm )
-  !   CALL mp_bcast ( ecutrho, ionode_id, world_comm )
-  !   CALL mp_bcast ( nr, ionode_id, world_comm )
-  !   CALL mp_bcast ( celvol, ionode_id, world_comm )
-  !   CALL mp_bcast ( al, ionode_id, world_comm )
-  !   CALL mp_bcast ( a, ionode_id, world_comm )
-  !   CALL mp_bcast ( adot, ionode_id, world_comm )
-  !   CALL mp_bcast ( recvol, ionode_id, world_comm )
-  !   CALL mp_bcast ( bl, ionode_id, world_comm )
-  !   CALL mp_bcast ( b, ionode_id, world_comm )
-  !   CALL mp_bcast ( bdot, ionode_id, world_comm )
+    CALL mp_bcast ( ns, ionode_id, world_comm )
+    CALL mp_bcast ( ng, ionode_id, world_comm )
+    CALL mp_bcast ( ntran, ionode_id, world_comm )
+    CALL mp_bcast ( cell_symmetry, ionode_id, world_comm )
+    CALL mp_bcast ( na, ionode_id, world_comm )
+    CALL mp_bcast ( ecutrho, ionode_id, world_comm )
+    CALL mp_bcast ( nr, ionode_id, world_comm )
+    CALL mp_bcast ( celvol, ionode_id, world_comm )
+    CALL mp_bcast ( al, ionode_id, world_comm )
+    CALL mp_bcast ( a, ionode_id, world_comm )
+    CALL mp_bcast ( adot, ionode_id, world_comm )
+    CALL mp_bcast ( recvol, ionode_id, world_comm )
+    CALL mp_bcast ( bl, ionode_id, world_comm )
+    CALL mp_bcast ( b, ionode_id, world_comm )
+    CALL mp_bcast ( bdot, ionode_id, world_comm )
 
-  !   IF ( ns .NE. nspin ) CALL errore ( 'write_cd', 'ns', 1 )
-  !   IF ( ng .NE. ngm_g ) CALL errore ( 'write_cd', 'ng', 1 )
-  !   IF ( na .NE. nat ) CALL errore ( 'write_cd', 'na', 1 )
-  !   IF ( nr ( 1 ) .NE. dfftp%nr1 .OR. nr ( 2 ) .NE. dfftp%nr2 .OR. nr ( 3 ) .NE. dfftp%nr3 ) &
-  !     CALL errore ( 'write_cd', 'nr', 1 )
-  !   IF ( abs ( celvol - omega ) .GT. eps6 ) &
-  !     CALL errore ( 'write_cd', 'unit cell volume', 1 )
-  !   xdel = 0.0D0
-  !   DO i = 1, 3
-  !     DO j = 1, 3
-  !       xdel = xdel + abs ( al * a ( j, i ) - alat * at ( j, i ) )
-  !     ENDDO
-  !   ENDDO
-  !   IF ( xdel .GT. eps6 ) &
-  !     CALL errore ( 'write_cd', 'direct lattice vectors', 1 )
-  !   xdel = 0.0D0
-  !   DO i = 1, 3
-  !     DO j = 1, 3
-  !       xdel = xdel + abs ( bl * b ( j, i ) - tpiba * bg ( j, i ) )
-  !     ENDDO
-  !   ENDDO
-  !   IF ( xdel .GT. eps6 ) &
-  !     CALL errore ( 'write_cd', 'reciprocal lattice vectors', 1 )
+    IF ( ns .NE. nspin ) CALL errore ( 'write_cd', 'ns', 1 )
+    IF ( ng .NE. ngm_g ) CALL errore ( 'write_cd', 'ng', 1 )
+    IF ( na .NE. nat ) CALL errore ( 'write_cd', 'na', 1 )
+    IF ( nr ( 1 ) .NE. dfftp%nr1 .OR. nr ( 2 ) .NE. dfftp%nr2 .OR. nr ( 3 ) .NE. dfftp%nr3 ) &
+      CALL errore ( 'write_cd', 'nr', 1 )
+    IF ( abs ( celvol - omega ) .GT. eps6 ) &
+      CALL errore ( 'write_cd', 'unit cell volume', 1 )
+    xdel = 0.0D0
+    DO i = 1, 3
+      DO j = 1, 3
+        xdel = xdel + abs ( al * a ( j, i ) - alat * at ( j, i ) )
+      ENDDO
+    ENDDO
+    IF ( xdel .GT. eps6 ) &
+      CALL errore ( 'write_cd', 'direct lattice vectors', 1 )
+    xdel = 0.0D0
+    DO i = 1, 3
+      DO j = 1, 3
+        xdel = xdel + abs ( bl * b ( j, i ) - tpiba * bg ( j, i ) )
+      ENDDO
+    ENDDO
+    IF ( xdel .GT. eps6 ) &
+      CALL errore ( 'write_cd', 'reciprocal lattice vectors', 1 )
 
-  !   ALLOCATE ( gvec ( 3, ng ) )
-  !   IF ( real_or_complex .EQ. 1 ) THEN
-  !     ALLOCATE ( rhogr ( ng, ns ) )
-  !   ELSE
-  !     ALLOCATE ( rhogc ( ng, ns ) )
-  !   ENDIF
-  !   ALLOCATE ( rhog ( ng, ns ) )
+    ALLOCATE ( gvec ( 3, ng ) )
+    IF ( real_or_complex .EQ. 1 ) THEN
+      ALLOCATE ( rhogr ( ng, ns ) )
+    ELSE
+      ALLOCATE ( rhogc ( ng, ns ) )
+    ENDIF
+    ALLOCATE ( rhog ( ng, ns ) )
 
-  !   IF ( ionode ) THEN
-  !     READ ( iu )
-  !     READ ( iu )
-  !     READ ( iu )
-  !     READ ( iu ) nrecord
-  !     ig = 1
-  !     DO irecord = 1, nrecord
-  !       READ ( iu ) ng_irecord
-  !       READ ( iu ) ( ( gvec ( ir, jg ), ir = 1, 3 ), jg = ig, ig + ng_irecord - 1 )
-  !       ig = ig + ng_irecord
-  !     ENDDO
-  !     READ ( iu ) nrecord
-  !     ig = 1
-  !     DO irecord = 1, nrecord
-  !       READ ( iu ) ng_irecord
-  !       IF ( real_or_complex .EQ. 1 ) THEN
-  !         READ ( iu ) ( ( rhogr ( jg, is ), jg = ig, ig + ng_irecord - 1 ), is = 1, ns )
-  !       ELSE
-  !         READ ( iu ) ( ( rhogc ( jg, is ), jg = ig, ig + ng_irecord - 1 ), is = 1, ns )
-  !       ENDIF
-  !       ig = ig + ng_irecord
-  !     ENDDO
-  !     DO is = 1, ns
-  !       IF ( real_or_complex .EQ. 1 ) THEN
-  !         DO ig = 1, ng
-  !           rhog ( ig, is ) = CMPLX ( rhogr ( ig, is ), 0.0D0, KIND=dp )
-  !         ENDDO
-  !       ELSE
-  !         DO ig = 1, ng
-  !           rhog ( ig, is ) = rhogc ( ig, is )
-  !         ENDDO
-  !       ENDIF
-  !     ENDDO
-  !     CLOSE ( unit = iu, status = 'keep' )
-  !   ENDIF
+    IF ( ionode ) THEN
+      READ ( iu )
+      READ ( iu )
+      READ ( iu )
+      READ ( iu ) nrecord
+      ig = 1
+      DO irecord = 1, nrecord
+        READ ( iu ) ng_irecord
+        READ ( iu ) ( ( gvec ( ir, jg ), ir = 1, 3 ), jg = ig, ig + ng_irecord - 1 )
+        ig = ig + ng_irecord
+      ENDDO
+      READ ( iu ) nrecord
+      ig = 1
+      DO irecord = 1, nrecord
+        READ ( iu ) ng_irecord
+        IF ( real_or_complex .EQ. 1 ) THEN
+          READ ( iu ) ( ( rhogr ( jg, is ), jg = ig, ig + ng_irecord - 1 ), is = 1, ns )
+        ELSE
+          READ ( iu ) ( ( rhogc ( jg, is ), jg = ig, ig + ng_irecord - 1 ), is = 1, ns )
+        ENDIF
+        ig = ig + ng_irecord
+      ENDDO
+      DO is = 1, ns
+        IF ( real_or_complex .EQ. 1 ) THEN
+          DO ig = 1, ng
+            rhog ( ig, is ) = CMPLX ( rhogr ( ig, is ), 0.0D0, KIND=dp )
+          ENDDO
+        ELSE
+          DO ig = 1, ng
+            rhog ( ig, is ) = rhogc ( ig, is )
+          ENDDO
+        ENDIF
+      ENDDO
+      CLOSE ( unit = iu, status = 'keep' )
+    ENDIF
 
-  !   IF ( real_or_complex .EQ. 1 ) THEN
-  !     DEALLOCATE ( rhogr )
-  !   ELSE
-  !     DEALLOCATE ( rhogc )
-  !   ENDIF
+    IF ( real_or_complex .EQ. 1 ) THEN
+      DEALLOCATE ( rhogr )
+    ELSE
+      DEALLOCATE ( rhogc )
+    ENDIF
 
-  !   IF ( ionode ) THEN
-  !     DO is = 1, ns
-  !       DO ig = 1, ng
-  !         rhog ( ig, is ) = rhog ( ig, is ) / CMPLX ( omega, 0.0D0, KIND=dp )
-  !       ENDDO
-  !     ENDDO
-  !   ENDIF
+    IF ( ionode ) THEN
+      DO is = 1, ns
+        DO ig = 1, ng
+          rhog ( ig, is ) = rhog ( ig, is ) / CMPLX ( omega, 0.0D0, KIND=dp )
+        ENDDO
+      ENDDO
+    ENDIF
 
-  !   CALL mp_bcast ( gvec, ionode_id, world_comm )
-  !   CALL mp_bcast ( rhog, ionode_id, world_comm )
+    CALL mp_bcast ( gvec, ionode_id, world_comm )
+    CALL mp_bcast ( rhog, ionode_id, world_comm )
 
-  !   fg = 0
-  !   DO ig = 1, ngm
-  !     IF ( ( gvec ( 1, ig_l2g ( ig ) ) .NE. mill ( 1, ig ) ) .OR. &
-  !          ( gvec ( 2, ig_l2g ( ig ) ) .NE. mill ( 2, ig ) ) .OR. &
-  !          ( gvec ( 3, ig_l2g ( ig ) ) .NE. mill ( 3, ig ) ) ) &
-  !       fg = fg + 1
-  !   ENDDO
-  !   CALL mp_sum ( fg, intra_pool_comm )
-  !   IF ( fg .GT. 0 ) THEN
-  !     IF ( ionode ) WRITE ( 6, 101 )
-  !     ALLOCATE ( rhog_unsrt ( ng, ns ) )
-  !     rhog_unsrt ( :, : ) = rhog ( :, : )
-  !     rhog ( :, : ) = ( 0.0D0, 0.0D0 )
-  !     DO ig = 1, ng
-  !       DO jg = 1, ngm
-  !         IF ( ( mill ( 1, jg ) .EQ. gvec ( 1, ig ) ) .AND. &
-  !              ( mill ( 2, jg ) .EQ. gvec ( 2, ig ) ) .AND. &
-  !              ( mill ( 3, jg ) .EQ. gvec ( 3, ig ) ) ) THEN
-  !           DO is = 1, ns
-  !             rhog ( ig_l2g ( jg ), is ) = rhog_unsrt ( ig, is )
-  !           ENDDO
-  !         ENDIF
-  !       ENDDO
-  !     ENDDO
-  !     DEALLOCATE ( rhog_unsrt )
-  !     CALL mp_sum ( rhog, intra_pool_comm )
-  !     gvec ( :, : ) = 0
-  !     DO ig = 1, ngm
-  !       gvec ( 1, ig_l2g ( ig ) ) = mill ( 1, ig )
-  !       gvec ( 2, ig_l2g ( ig ) ) = mill ( 2, ig )
-  !       gvec ( 3, ig_l2g ( ig ) ) = mill ( 3, ig )
-  !     ENDDO
-  !     CALL mp_sum ( gvec, intra_pool_comm )
-  !   ENDIF
+    fg = 0
+    DO ig = 1, ngm
+      IF ( ( gvec ( 1, ig_l2g ( ig ) ) .NE. mill ( 1, ig ) ) .OR. &
+           ( gvec ( 2, ig_l2g ( ig ) ) .NE. mill ( 2, ig ) ) .OR. &
+           ( gvec ( 3, ig_l2g ( ig ) ) .NE. mill ( 3, ig ) ) ) &
+        fg = fg + 1
+    ENDDO
+    CALL mp_sum ( fg, intra_pool_comm )
+    IF ( fg .GT. 0 ) THEN
+      IF ( ionode ) WRITE ( 6, 101 )
+      ALLOCATE ( rhog_unsrt ( ng, ns ) )
+      rhog_unsrt ( :, : ) = rhog ( :, : )
+      rhog ( :, : ) = ( 0.0D0, 0.0D0 )
+      DO ig = 1, ng
+        DO jg = 1, ngm
+          IF ( ( mill ( 1, jg ) .EQ. gvec ( 1, ig ) ) .AND. &
+               ( mill ( 2, jg ) .EQ. gvec ( 2, ig ) ) .AND. &
+               ( mill ( 3, jg ) .EQ. gvec ( 3, ig ) ) ) THEN
+            DO is = 1, ns
+              rhog ( ig_l2g ( jg ), is ) = rhog_unsrt ( ig, is )
+            ENDDO
+          ENDIF
+        ENDDO
+      ENDDO
+      DEALLOCATE ( rhog_unsrt )
+      CALL mp_sum ( rhog, intra_pool_comm )
+      gvec ( :, : ) = 0
+      DO ig = 1, ngm
+        gvec ( 1, ig_l2g ( ig ) ) = mill ( 1, ig )
+        gvec ( 2, ig_l2g ( ig ) ) = mill ( 2, ig )
+        gvec ( 3, ig_l2g ( ig ) ) = mill ( 3, ig )
+      ENDDO
+      CALL mp_sum ( gvec, intra_pool_comm )
+    ENDIF
 
-  !   DO is = 1, ns
-  !     DO ig = 1, ngm
-  !       rho%of_g ( ig, is ) = rhog ( ig_l2g ( ig ), is )
-  !     ENDDO
-  !   ENDDO
+    DO is = 1, ns
+      DO ig = 1, ngm
+        rho%of_g ( ig, is ) = rhog ( ig_l2g ( ig ), is )
+      ENDDO
+    ENDDO
 
-  !   DEALLOCATE ( rhog )
+    DEALLOCATE ( rhog )
 
-  !   DO is = 1, ns
-  !     DO ig = 1, ngm
-  !       psic ( dfftp%nl ( ig ) ) = rho%of_g ( ig, is )
-  !     ENDDO
-  !     CALL invfft ( 'Rho', psic, dfftp )
-  !     DO ir = 1, dfftp%nnr
-  !       rho%of_r ( ir, is ) = psic ( ir )
-  !     ENDDO
-  !   ENDDO
+    DO is = 1, ns
+      DO ig = 1, ngm
+        psic ( dfftp%nl ( ig ) ) = rho%of_g ( ig, is )
+      ENDDO
+      CALL invfft ( 'Rho', psic, dfftp )
+      DO ir = 1, dfftp%nnr
+        rho%of_r ( ir, is ) = psic ( ir )
+      ENDDO
+    ENDDO
 
-  !   CALL create_directory ( output_dir_name )
+    ! CALL create_directory ( output_dir_name )
 
-  !   filename = TRIM ( output_dir_name ) // '/gvectors.dat'
+    ! filename = TRIM ( output_dir_name ) // '/gvectors.dat'
 
-  !   IF ( ionode ) THEN
-  !     CALL iotk_open_write ( iu, FILE = TRIM ( filename ), SKIP_ROOT = .TRUE., BINARY = .TRUE. )
-  !     CALL iotk_write_begin ( iu, "G-VECTORS" )
-  !     CALL iotk_write_attr ( attr, "nr1s", dfftp%nr1, FIRST = .TRUE. )
-  !     CALL iotk_write_attr ( attr, "nr2s", dfftp%nr2 )
-  !     CALL iotk_write_attr ( attr, "nr3s", dfftp%nr3 )
-  !     CALL iotk_write_attr ( attr, "gvect_number", ng )
-  !     CALL iotk_write_attr ( attr, "gamma_only", .FALSE. )
-  !     CALL iotk_write_attr ( attr, "units", "crystal" )
-  !     CALL iotk_write_empty ( iu, "INFO", ATTR = attr )
-  !     CALL iotk_write_dat ( iu, "g", gvec ( 1 : 3, 1 : ng ), COLUMNS = 3 )
-  !     CALL iotk_write_end ( iu, "G-VECTORS" )
-  !     CALL iotk_close_write ( iu )
-  !   ENDIF
+    ! IF ( ionode ) THEN
+    !   CALL iotk_open_write ( iu, FILE = TRIM ( filename ), SKIP_ROOT = .TRUE., BINARY = .TRUE. )
+    !   CALL iotk_write_begin ( iu, "G-VECTORS" )
+    !   CALL iotk_write_attr ( attr, "nr1s", dfftp%nr1, FIRST = .TRUE. )
+    !   CALL iotk_write_attr ( attr, "nr2s", dfftp%nr2 )
+    !   CALL iotk_write_attr ( attr, "nr3s", dfftp%nr3 )
+    !   CALL iotk_write_attr ( attr, "gvect_number", ng )
+    !   CALL iotk_write_attr ( attr, "gamma_only", .FALSE. )
+    !   CALL iotk_write_attr ( attr, "units", "crystal" )
+    !   CALL iotk_write_empty ( iu, "INFO", ATTR = attr )
+    !   CALL iotk_write_dat ( iu, "g", gvec ( 1 : 3, 1 : ng ), COLUMNS = 3 )
+    !   CALL iotk_write_end ( iu, "G-VECTORS" )
+    !   CALL iotk_close_write ( iu )
+    ! ENDIF
 
-  !   DEALLOCATE ( gvec )
+    DEALLOCATE ( gvec )
 
-  !   CALL write_rho ( output_dir_name, rho%of_r, nspin )
+    ! CALL write_rho ( output_dir_name, rho%of_r, nspin )
+    prefix = TRIM(prefix)//"_cd"    
+    CALL write_scf(rho, nspin)
 
-  !   RETURN
+    RETURN
 
-  !   101 FORMAT ( 5X, "WARNING: reordering G-vectors" )
+    101 FORMAT ( 5X, "WARNING: reordering G-vectors" )
 
-  ! END SUBROUTINE write_cd
+  END SUBROUTINE write_cd
 
   !-------------------------------------------------------------------------------
 
